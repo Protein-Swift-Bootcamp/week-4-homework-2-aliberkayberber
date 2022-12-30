845df21e816f925caaa339716ae008e7970ac67b
aliberkayberber <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>
2022-12-30 22:31:51 +0300

    creat xcode file

---
+++ Week4Homework/Week4Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week4Homework
+//
+//  Created by Ali Berkay BERBER on 30.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
